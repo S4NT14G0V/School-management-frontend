5c5c8de1570ef43f73e2cd9e0ba319295c46b3ca
S4NT14G0V <100742956+S4NT14G0V@users.noreply.github.com>
2024-11-19 12:56:20 -0500

    Update LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -12,6 +12,7 @@ export default function LoginPage() {
         backgroundPosition: "center",
         backgroundClip: "cover",
         backgroundImage: `url(${Background})`,
+        backgroundSize: "cover",
       }}
     >
       <Login />
