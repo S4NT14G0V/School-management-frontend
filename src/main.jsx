15fc8dd3e925706897c856adf87bbf09cd7f6d49
S4NT14G0V <100742956+S4NT14G0V@users.noreply.github.com>
2024-11-19 12:43:35 -0500

    statistics implement

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="457746752857-n3b8nimk2pbdb7vuegsl26h8nudbpbdo.apps.googleusercontent.com">
       <App/>
+      <SpeedInsights />
     </GoogleOAuthProvider>
   </StrictMode>,
 )
