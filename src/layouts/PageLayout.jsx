5c030feddecb0a83befeb77ea1ece3cdf62ef39e
S4NT14G0V <100742956+S4NT14G0V@users.noreply.github.com>
2025-02-01 19:25:51 -0500

    Update PageLayout.jsx

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -50,6 +50,12 @@ const PageLayout = ({ children }) => {
         notification.open({
           message: message.title,
           description: message.message,
+          duration: 10,
+          style: {
+            backgroundColor: '#f0f0f0', // Color de fondo
+            borderLeft: '3px solid rgb(76, 0, 253)', // Borde
+            color: '#000', // Color del texto
+          },
         });
       });
       setMessageNotifications([]); // Limpiar las notificaciones después de mostrarlas
