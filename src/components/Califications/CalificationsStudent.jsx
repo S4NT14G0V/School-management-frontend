5c6029089021335f03ddad338d9af86ee783d38e
S4NT14G0V <100742956+S4NT14G0V@users.noreply.github.com>
2025-03-13 17:47:11 -0500

    Update CalificationsStudent.jsx

--- src/components/Califications/CalificationsStudent.jsx
+++ src/components/Califications/CalificationsStudent.jsx
@@ -1,119 +1,137 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { Table, Divider } from "antd";
-import { getCalificationsByEmail } from "@services/califications";
-import { MESSAGES_ERROR } from "@config/constants";
+import {
+  getCalificationsSummaryByEmail,
+  getCalificationsByEmail,
+  downloadCalifications,
+} from "@services/califications";
+import { validateTeachersAdmins } from "@services/userService";
+
 import "./CalificationsEdit.css";
 
 export default function CalificationsStudent() {
-  const [dataSource, setDataSource] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [subjects, setSubjects] = useState([]);
+  const [assessments, setAssessments] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  // Función para obtener las calificaciones
-  const fetchCalificationsStudent = useCallback(async () => {
+  const rowColors = useMemo(() => ["#0058ca67", "#72b0ff99"], []);
+
+  const [validUser, setValidUser] = useState(false);
+
+  const validate = async () => {
+    try {
+      const valid = await validateTeachersAdmins();
+      if (valid) {
+        setValidUser(true);
+      } else {
+        throw new Error("User not valid.");
+      }
+    } catch (error) {
+      //console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    validate();
+  }, []);
+
+  const download = async () => {
+    try {
+      if (validUser) {
+        downloadCalifications();
+      }
+    } catch (error) {
+      //console.error("Error fetching data:", error);
+    }
+  };
+
+  const fetchData = useCallback(async () => {
     setLoading(true);
     try {
-      const califications = await getCalificationsByEmail();
-      return califications;
+      const [subjectsData, assessmentsData] = await Promise.all([
+        getCalificationsSummaryByEmail(),
+        getCalificationsByEmail(),
+      ]);
+
+      setSubjects(
+        subjectsData.map((item) => ({
+          studentId: item.student.id,
+          subjects: item.subjects.map((sub) => ({
+            subject: sub.subject,
+            average: sub.average,
+          })),
+        }))
+      );
+
+      setAssessments(
+        assessmentsData.reduce((acc, calification) => {
+          const studentId = calification.student.id;
+          const studentInfo = calification.student;
+
+          const assessment = {
+            subjectName: calification.assesment.classes.subject.name,
+            description: calification.assesment.description,
+            percent: calification.assesment.percent,
+            calification: calification.calification,
+            group: `${calification.assesment.classes.group.grade}-${calification.assesment.classes.group.variant}`,
+          };
+
+          const studentIndex = acc.findIndex(
+            (item) => item.studentId === studentId
+          );
+          if (studentIndex === -1) {
+            acc.push({ studentId, studentInfo, assessments: [assessment] });
+          } else {
+            acc[studentIndex].assessments.push(assessment);
+          }
+
+          return acc;
+        }, [])
+      );
     } catch (error) {
-      console.error(MESSAGES_ERROR.STANDARD_ERROR_FETCHING, error);
-      return [];
+      //console.error("Error fetching data:", error);
     } finally {
       setLoading(false);
     }
   }, []);
 
-  // Transformar y agrupar datos
-  const transformData = useCallback((califications) => {
-    const groupedData = {};
-
-    califications.forEach((studentCalif) => {
-      const studentName = `${studentCalif.student?.name} ${studentCalif.student?.lastname}`;
-      const subjectName = studentCalif.assesment?.classes?.subject?.name || "Sin materia";
-      const description = studentCalif.assesment?.description || "No tienes calificaciones";
-      const percent = studentCalif.assesment?.percent || 0;
-      const calification = studentCalif.calification || 0;
-
-      if (!groupedData[studentName]) {
-        groupedData[studentName] = {};
-      }
-      if (!groupedData[studentName][subjectName]) {
-        groupedData[studentName][subjectName] = [];
-      }
-
-      groupedData[studentName][subjectName].push({
-        key: studentCalif.id,
-        description,
-        percent,
-        calification,
-      });
-    });
-
-    return groupedData;
-  }, []);
-
-  // Cargar datos al montar el componente o al actualizar
-  const loadData = useCallback(async () => {
-    try {
-      const userData = await fetchCalificationsStudent();
-      if (userData.length > 0) {
-        const groupedData = transformData(userData);
-        setDataSource(groupedData);
-      }
-    } catch (error) {
-      console.error(MESSAGES_ERROR.STANDARD_ERROR_FETCHING, error);
-    }
-  }, [fetchCalificationsStudent, transformData]);
-
   useEffect(() => {
-    loadData();
-  }, [loadData]);
+    fetchData();
+  }, [fetchData]);
 
-  // Calcular totales
-  const calculateTotals = useCallback((data) => {
-    const totalPercent = data.reduce((sum, item) => sum + item.percent, 0);
-    const weightedSum = data.reduce(
-      (sum, item) => sum + item.calification * (item.percent / 100),
-      0
-    );
-    const weightedEval = (weightedSum / totalPercent) * 100;
-    const weightedAverage = totalPercent > 0 ? weightedEval : 0;
-    const total = `${totalPercent}`;
+  const summaryColumns = [
+    { title: "Materia", dataIndex: "subject", key: "subject", width: "70%" },
+    {
+      title: "Promedio General",
+      dataIndex: "average",
+      key: "average",
+      render: (text) => (text !== undefined ? text.toFixed(2) : "0.00"),
+      width: "30%",
+    },
+  ];
 
-    return { total, weightedAverage };
-  }, []);
+  const assessmentColumns = [
+    {
+      title: "Tarea",
+      dataIndex: "description",
+      key: "description",
+      width: "70%",
+    },
+    {
+      title: "Porcentaje",
+      dataIndex: "percent",
+      key: "percent",
+      width: "15%",
+      render: (text) => `${text}%`,
+    },
+    {
+      title: "Calificación",
+      dataIndex: "calification",
+      key: "calification",
+      width: "15%",
+    },
+  ];
 
-  const columns = useMemo(
-    () => [
-      { title: "Descripción", dataIndex: "description", key: "description" },
-      {
-        title: "Porcentaje",
-        dataIndex: "percent",
-        key: "percent",
-        render: (text, record) => (
-          <span
-            style={{
-              textAlign: "center",
-              display: "block",
-              fontWeight: String(record.key).includes("total") ? "bold" : "normal",
-            }}
-          >
-            {text}%
-          </span>
-        ),
-      },
-      {
-        title: "Calificación",
-        dataIndex: "calification",
-        key: "calification",
-        render: (_, record) => (
-          <span style={{ textAlign: "center", display: "block" }}>
-            {record.calification}
-          </span>
-        ),
-      },
-    ],
-    []
-  );
+  const showStudentInfo = assessments.length >= 2;
 
   return (
     <>
@@ -126,11 +144,22 @@ export default function CalificationsStudent() {
           paddingInline: "7.5%",
         }}
       >
+        {validUser && (
+          <button
+            style={{
+              width: "fit-content",
+              paddingInline: "20px",
+              fontSize: "0.8rem",
+            }}
+            onClick={() => download()}
+          >
+            Download
+          </button>
+        )}
         <button
-          onClick={loadData}
+          onClick={fetchData}
           style={{
             width: "80px",
-            height: "30px",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
@@ -146,48 +175,98 @@ export default function CalificationsStudent() {
           width: "85%",
           height: "100%",
           overflowY: "auto",
-          paddingInline: "20px",
+          paddingInline: "0",
         }}
       >
-        {Object.keys(dataSource).map((student) => (
-          <div key={student}>
-            <Divider orientation="left" style={{ fontSize: "18px" }}>
-              {student}
-            </Divider>
-            {Object.keys(dataSource[student]).map((subject) => {
-              const subjectData = dataSource[student][subject];
-              const { total, weightedAverage } = calculateTotals(subjectData);
-
-              const dataWithTotal = [
-                ...subjectData,
-                {
-                  key: `${subject}-total`,
-                  description: <strong>Total</strong>,
-                  percent: <strong>{total}</strong>,
-                  calification: <strong>{weightedAverage.toFixed(2)}</strong>,
+        {assessments.map(({ studentId, studentInfo, assessments }) => (
+          <div key={studentId} style={{ padding: "2rem" }}>
+            {showStudentInfo && (
+              <Divider
+                orientation="left"
+                style={{
+                  fontSize: "18px",
+                  position: "relative",
+                  paddingLeft: "1rem",
+                }}
+              >
+                <span
+                  style={{
+                    marginLeft: "1rem",
+                    fontSize: "14px",
+                    backgroundColor: "#0058ca",
+                    cursor: "default",
+                    color: "white",
+                    padding: "0.25rem 0.5rem",
+                    borderRadius: "0.25rem",
+                    position: "absolute",
+                    left: 0,
+                  }}
+                >
+                  {assessments[0]?.group}
+                </span>
+                {`${studentInfo.name.charAt(0).toUpperCase()}${studentInfo.name
+                  .slice(1)
+                  .toLowerCase()} 
+                  ${studentInfo.lastname
+                    .split(" ")
+                    .map(
+                      (name) =>
+                        name.charAt(0).toUpperCase() +
+                        name.slice(1).toLowerCase()
+                    )
+                    .join(" ")}`}
+              </Divider>
+            )}
+            <Table
+              bordered
+              dataSource={
+                subjects.find((s) => s.studentId === studentId)?.subjects || []
+              }
+              columns={summaryColumns}
+              loading={loading}
+              pagination={false}
+              rowKey="subject"
+              expandable={{
+                expandedRowRender: (record) => {
+                  const subjectAssessments = assessments.filter(
+                    (assessment) => assessment.subjectName === record.subject
+                  );
+                  return (
+                    <Table
+                      bordered
+                      dataSource={subjectAssessments}
+                      columns={assessmentColumns}
+                      pagination={false}
+                      rowKey="description"
+                      style={{ width: "100%" }}
+                    />
+                  );
                 },
-              ];
-
-              return (
-                <div key={subject}>
-                  <h3 style={{ display: "block", textAlign: "start", fontWeight: "bold" }}>
-                    {subject}
-                  </h3>
-                  <Table
-                    bordered
-                    dataSource={dataWithTotal}
-                    columns={columns}
-                    loading={loading}
-                    rowClassName="editable-row"
-                    pagination={false}
-                    style={{ marginBottom: "20px" }}
-                  />
-                </div>
-              );
-            })}
+              }}
+              rowClassName={(_, index) => `subject-row-${index % 2}`}
+              style={{ width: "100%" }}
+            />
           </div>
         ))}
       </div>
+      <style>
+        {`
+          .ant-table-thead > tr > th {
+            background-color: #11538C !important;
+            color: white !important;
+            border-radius: 0 !important;
+          }
+          .subject-row-0 {
+            background-color: ${rowColors[0]} !important;
+          }
+          .subject-row-1 {
+            background-color: ${rowColors[1]} !important;
+          }
+          .ant-table-tbody > tr:hover > td {
+            background-color: inherit !important;
+          }
+        `}
+      </style>
     </>
   );
 }
